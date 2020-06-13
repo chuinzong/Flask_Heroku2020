288ee972c3d484c8baa7d51d41b140941a79ae07
chuinzong <66862430+chuinzong@users.noreply.github.com>
2020-06-13 16:20:45 +0800

    Add files via upload
    Reply

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot.models import *
 
 app = Flask(__name__)
 
@@ -18,11 +16,8 @@ line_bot_api = LineBotApi('db2W0M6n+YN84WscM6W9vVtUmji+oAVXKNs3BCFs1cfOoX0ng4m3j
 handler = WebhookHandler('91fec83ea2c09ab57ef0dd80e6be823e')
 # Channel secret 
 
-
-
 @app.route("/callback", methods=['POST'])
 def callback():
-
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
@@ -38,13 +33,55 @@ def callback():
 
     return 'OK'
 
+def getNews():
+	"""
+	建立一個抓最新消息的function
+	"""
+	import requests
+	import re
+	from bs4 import BeautifulSoup
 
+	url = 'https://www.ettoday.net/news/focus/3C%E5%AE%B6%E9%9B%BB/'
+	r = requests.get(url)
+	reponse = r.text
+
+	url_list = re.findall(r'<h3><a href="/news/[\d]*/[\d]*.htm" .*>.*</a>',reponse)
+
+	soup = BeautifulSoup(url_list[0])
+	url = 'https://fashion.ettoday.net/' + soup.find('a')['href']
+	title = soup.text
+
+
+	tmp = title + ': ' +url
+	return tmp
+	
+from bs4 import BeautifulSoup
+import requests
+def movie():
+    target_url = 'http://www.atmovies.com.tw/movie/next/0/'
+    print('Start parsing movie ...')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    res.encoding = 'utf-8'
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ""
+    for index, data in enumerate(soup.select('ul.filmListAll a')):
+        if index == 20:
+            break
+        title = data.text.replace('\t', '').replace('\r', '')
+        link = "http://www.atmovies.com.tw" + data['href']
+        content += '{}\n{}\n'.format(title, link)
+    return content
+	
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
- 
+    # 傳送文字
+    if event.message.text == '傳送文字':
+        message = TextSendMessage(getNews())
+	else:
+        message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
 
+	
 if __name__ == '__main__':
     app.run(debug=True)
