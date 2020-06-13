5b29545e672b0829fc937b0eed49ebd98329565d
chuinzong <66862430+chuinzong@users.noreply.github.com>
2020-06-13 17:06:12 +0800

    Add files via upload
    Movie

--- app.py
+++ app.py
@@ -57,35 +57,52 @@ def getNews():
 
 from bs4 import BeautifulSoup
 import requests
-def movie():
+def movie(num):
+    num = int(num)
     target_url = 'http://www.atmovies.com.tw/movie/next/0/'
     print('Start parsing movie ...')
     rs = requests.session()
     res = rs.get(target_url, verify=False)
     res.encoding = 'utf-8'
     soup = BeautifulSoup(res.text, 'html.parser')
-    content = ""
+    content = []
     for index, data in enumerate(soup.select('ul.filmListAll a')):
-        if index == 20:
+        if index == num:
             break
         title = data.text.replace('\t', '').replace('\r', '')
         link = "http://www.atmovies.com.tw" + data['href']
-        content += '{}\n{}\n'.format(title, link)
+        if len(title) == 0:
+            num += 1
+            continue
+        content += [[title, link]]
     return content
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 傳送文字
-    if event.message.text == '傳送文字':
-        message = TextSendMessage(getNews())
-    elif event.message.text == '電影':
-        message = TextSendMessage(movie())
-    elif event.message.text.startswith('貼圖'):
-        text = event.message.text
-        _, package_id, sticker_id = text.split('-')
-        message = StickerSendMessage(
-            package_id=int(package_id),
-            sticker_id=int(sticker_id)
+    if event.message.text.startswith('電影-'):
+        _, num = event.message.text.split('-')
+        print(num)
+        movie_info = movie(num)
+        print(movie_info)
+        columns_info = []
+        for m in movie_info:
+            colum = CarouselColumn(
+                    thumbnail_image_url='https://example.com/item1.jpg',
+                    title=m[0],
+                    text=m[0],
+                    actions=[
+                        URITemplateAction(
+                            label='前往觀看',
+                            uri=m[1]
+                        )
+                    ]
+                )
+            columns_info += [colum]
+        print(columns_info)        
+        message = TemplateSendMessage(
+            alt_text='新聞文章',
+            template=CarouselTemplate(columns= columns_info)
         )
     else:
         message = TextSendMessage(text=event.message.text)
